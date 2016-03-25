4ca3d72cc09e89350308ec2db6499ad03c6756af
naorton <naorton@gmail.com>
2016-03-24 22:30:42 -0500

    added in loop for finding all addresses on bus

--- mpr121/presentation.py
+++ mpr121/presentation.py
@@ -6,11 +6,18 @@ bus = smbus.SMBus(1)
 print 'Press Ctrl-C to quit'
 
 while True:
+
+	for i in xrange(0, 255):
+		bus.write_byte(i,1)
+		time.sleep(.1)
+		data = bus.read_byte(i)
+		print data
+
 	address = input('Enter an address: ')
 	command = input('Enter command: ')
 
 	bus.write_byte(address,command)
-	time.sleep(1)
-	data = bus.read_byte(address)
+	time.sleep(.1)
+	device_addr = bus.read_byte(address)
 
-	print data	
+	print device_addr
