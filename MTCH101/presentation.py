a97fb24032a9c45be942d790598ed845811b45c7
naorton <naorton@gmail.com>
2016-04-21 22:19:27 -0500

    tryin to get this silly led to work

--- MTCH101/presentation.py
+++ MTCH101/presentation.py
@@ -14,10 +14,9 @@ print 'Press Ctrl-C to quit'
 while True:
 
     device_output = bus.read_byte(0x9)
-    print device_output
 
     if device_output == 1:
-
+    	print device_output
     	GPIO.setup(4, GPIO.HIGH)
     	time.sleep(.5)
     	GPIO.setup(4, GPIO.LOW)
