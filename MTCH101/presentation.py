2b5b65339c08359db0ce87a0ab629c709b5ae898
naorton <naorton@gmail.com>
2016-04-21 21:59:45 -0500

    trying to blink led

--- MTCH101/presentation.py
+++ MTCH101/presentation.py
@@ -3,9 +3,8 @@ import time
 import RPi.GPIO as GPIO
 
 bus = smbus.SMBus(1)
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
 
 
 bus = smbus.SMBus(1)
@@ -14,11 +13,11 @@ print 'Press Ctrl-C to quit'
 
 while True:
 
-    device_addr = bus.read_byte(0x9)
-    print device_addr
+    device_output = bus.read_byte(0x9)
+    print device_output
 
-    if device_addr == 1:
+    if device_output == 1:
 
-    	GPIO.setup(17, GPIO.HIGH)
+    	GPIO.setup(7, GPIO.HIGH)
     	time.sleep(.5)
-    	GPIO.setup(17, GPIO.LOW)
+    	GPIO.setup(7, GPIO.LOW)
