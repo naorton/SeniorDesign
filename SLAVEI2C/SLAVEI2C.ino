5c5e08c054fb825827468912d2b545be70ca6c4e
tdastle <tdastle@wichita.edu>
2016-04-24 21:13:56 -0500

    Updated slavei2c.  Changed to pulling high to low.

--- SLAVEI2C/SLAVEI2C.ino
+++ SLAVEI2C/SLAVEI2C.ino
@@ -4,7 +4,7 @@ const int busID = 9; // Bus address ID
 const int triggerPin = 3; // Pin on the attiny85 used for sensor trigger
 bool triggerState = true; // Boolean variable passed to master to indicate state of sensor 
 const int ledPin =  1; // Pin on the attiny85 used for sensor trigger led notification
-const int statemaxCount = 5000; // Delay between state change
+const int statemaxCount = 2500; // Delay between state change
 int stateCount = 0; // Counter used in state change
 
 void setup()                    
@@ -12,6 +12,7 @@ void setup()
    pinMode(triggerPin, INPUT); // Attiny 85 trigger pin set as input
    digitalWrite(triggerPin, HIGH); // Attiny 85 trigger pin set high, grounding pin triggers state change
    pinMode(ledPin, OUTPUT); // Attiny85 pin set as output, used for LED notification
+   digitalWrite(ledPin, HIGH);
    TinyWireS.begin(busID); // Open connection on bus as slave
    TinyWireS.onRequest(requestEvent); // Method used on requestFrom call from master
 }
@@ -19,9 +20,9 @@ void setup()
 void requestEvent()
 {
   if(triggerState)
-    TinyWireS.send('0');
+    TinyWireS.send(0);
   else
-    TinyWireS.send('1');
+    TinyWireS.send(1);
 }
 
 void loop()       
@@ -30,7 +31,7 @@ void loop()
      if(stateCount < statemaxCount)
        stateCount++;
      if(stateCount == statemaxCount){
-       digitalWrite(ledPin, HIGH);
+       digitalWrite(ledPin, LOW);
        triggerState = false;
      }  
    }
@@ -38,7 +39,7 @@ void loop()
      if(stateCount > 0)
        stateCount--;
      if(stateCount == 0){
-       digitalWrite(ledPin, LOW);
+       digitalWrite(ledPin, HIGH);
        triggerState = true;
      }
    }
