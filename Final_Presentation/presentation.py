730f65a94c89c07a2abfbc3f5dc9d545c497ca30
naorton <naorton@gmail.com>
2016-05-01 23:17:58 -0500

    fixed problem with sensor id, now trying with except and pass

--- Final_Presentation/presentation.py
+++ Final_Presentation/presentation.py
@@ -4,7 +4,11 @@ import requests
 #import RPi.GPIO as GPIO
 
 sensorLocation = {
-		 0x09 : 'A0',	
+
+		 0x09 : 'A0',
+		 0x0A : 'A2',
+		 0x0B : 'A4',
+		 0x0C : 'A6'
 }
 
 bus = smbus.SMBus(1)
@@ -18,7 +22,10 @@ while True:
 
 	for sensor in sensorLocation:
 
-		sensor_output = bus.read_byte(sensor)
+		try:
+			sensor_output = bus.read_byte(sensor)
+		except:
+			pass
 
 		if sensor_output == 1:
 			requests.get('http://raspberrypi/locations/%s' % (sensorLocation[sensor]))
